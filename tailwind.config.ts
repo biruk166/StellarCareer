3135bc3b0e483f475099cb6cbafd6895e8f30619
biruk166 <birukch4@gmail.com>
2026-05-14 16:40:49 -0400

    Intitial setup

---
+++ tailwind.config.ts
@@ -0,0 +1,98 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      // Add your custom colors, fonts, spacing, etc. here
+      colors: {
+        // color link: https://www.realtimecolors.com/?colors=0b0e0e-f1f9f9-175454-00c8ff-2fe9e9&fonts=Inter-Inter
+        // fonts https://fonts.google.com/specimen/Cairo?preview.lang=en_Latn
+        text: {
+          50: "var(--text-50)",
+          100: "var(--text-100)",
+          200: "var(--text-200)",
+          300: "var(--text-300)",
+          400: "var(--text-400)",
+          500: "var(--text-500)",
+          600: "var(--text-600)",
+          700: "var(--text-700)",
+          800: "var(--text-800)",
+          900: "var(--text-900)",
+          950: "var(--text-950)",
+        },
+        danger: {
+          50:  "danger(--text-50)",
+          100: "danger(--text-100)",
+          200: "danger(--text-200)",
+          300: "danger(--text-300)",
+          400: "danger(--text-400)",
+          500: "danger(--text-500)",
+          600: "danger(--text-600)",
+          700: "danger(--text-700)",
+          800: "danger(--text-800)",
+          900: "danger(--text-900)",
+          950: "danger(--text-950)",
+        },
+        success: {
+          50:  "success(--text-50)",
+          100: "success(--text-100)",
+          200: "success(--text-200)",
+          300: "success(--text-300)",
+          400: "success(--text-400)",
+          500: "success(--text-500)",
+          600: "success(--text-600)",
+          700: "success(--text-700)",
+          800: "success(--text-800)",
+          900: "success(--text-900)",
+          950: "success(--text-950)",
+        },
+        background: {
+          50:  "var(--bg-50)",
+          100: "var(--bg-100)",
+          200: "var(--bg-200)",
+          300: "var(--bg-300)",
+          400: "var(--bg-400)",
+          500: "var(--bg-500)",
+          600: "var(--bg-600)",
+          700: "var(--bg-700)",
+          800: "var(--bg-800)",
+          900: "var(--bg-900)",
+          950: "var(--bg-950)",
+        },
+        primary: {
+          50: "var(--primary-50)",
+          100: "var(--primary-100)",
+          200: "var(--primary-200)",
+          300: "var(--primary-300)",
+          400: "var(--primary-400)",
+          500: "var(--primary-500)",
+          600: "var(--primary-600)",
+          700: "var(--primary-700)",
+          800: "var(--primary-800)",
+          900: "var(--primary-900)",
+          950: "var(--primary-950)",
+        },
+        secondary: {
+          50: "var(--secondary-50)",
+          100: "var(--secondary-100)",
+          200: "var(--secondary-200)",
+          300: "var(--secondary-300)",
+          400: "var(--secondary-400)",
+          500: "var(--secondary-500)",
+          600: "var(--secondary-600)",
+          700: "var(--secondary-700)",
+          800: "var(--secondary-800)",
+          900: "var(--secondary-900)",
+          950: "var(--secondary-950)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
